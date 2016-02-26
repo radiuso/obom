260d36c983d551457292000340e510dad3fba4b1
Limarion <rachid.delory@gmail.com>
2016-02-26 11:43:25 +0100

    Add updating pois systeme with handlers and cron

---
+++ server/components/cron/dailyCron.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import _ from 'lodash';
+import GLHandler from '../../components/sourceHandlers/grandLyonHandler.js';
+var CronJob = require('cron').CronJob;
+
+var job = new CronJob({
+  cronTime: '00 00 06 * * *',
+  onTick: function() {
+    /*
+     * Runs every day
+     * at 06:00:00 AM.
+     */
+     GLHandler.populate();
+  },
+  start: true,
+  timeZone: 'France/Paris'
+});
---
+++ server/components/sourceHandlers/grandLyonHandler.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var _ = require('lodash');
+import Poi from '../../api/poi/poi.model';
+import request from 'request';
+
+
+function saveUpdates(updates) {
+  return function(entity) {
+    var updated = _.merge(entity, updates);
+    return updated.saveAsync()
+      .spread(updated => {
+        return updated;
+      });
+  };
+}
+
+
+module.exports = {
+  /**
+   * Get the Poi JSON source then populate the database.
+   *
+   * @return {String}
+   */
+  populate: function() {
+    console.log("Populating database ... ");
+    request('https://download.data.grandlyon.com/ws/rdata/sit_sitra.sittourisme/all.json', function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var pois = JSON.parse(body);
+
+        pois.values.forEach(function(poi) {
+          // If poi has no restauration type property we do nothing
+          if (poi.type !== 'RESTAURATION')
+            return;
+
+          // Populate the new poi model
+          var newPoi = new Poi(poi);
+          newPoi._id = poi.id_sitra1;
+          newPoi.name = poi.nom;
+          newPoi.tags = poi.type_detail.split(';');
+          newPoi.city = poi.commune;
+          newPoi.adress = poi.adresse;
+          newPoi.phone = poi.telephone;
+          newPoi.minRate = poi.tarifsmin;
+          newPoi.open = poi.ouverture;
+          newPoi.email = poi.email;
+
+          // Convert the Model instance to a simple object using Model's 'toObject' function
+          // to prevent weirdness like infinite looping...
+          var upsertData = newPoi.toObject();
+
+          // Delete the _id property, otherwise Mongo will return a "Mod on _id not allowed" error
+          delete upsertData._id;
+
+          // Do the upsert, which works like this: If no Poi document exists with
+          // _id = newPoi.id, then create a new doc using upsertData.
+          // Otherwise, update the existing doc with upsertData
+          Poi.update({
+            _id: newPoi._id
+          }, upsertData, {
+            upsert: true
+          }, function(err, doc) {
+            if(err)
+              console.log(doc);
+          });
+
+        });
+      }
+    })
+  }
+};
