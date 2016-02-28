a488119c7789b53d6aa852500485f91df99536a8
Radiuso <thomas.vandermeersch.21@gmail.com>
2016-02-28 16:33:28 +0100

    clean main
    clean main controller

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,38 +3,11 @@
 (function() {
 
 class MainController {
+  constructor() {
 
-  constructor($http, $scope, socket, ThingsService) {
-    this.$http = $http;
-    this.awesomeThings = [];
-    this.thingsService = ThingsService;
-
-    this.thingsService.getAll().then(response => {
-      this.awesomeThings = response.data;
-      socket.syncUpdates('thing', this.awesomeThings);
-    });
-
-    $scope.$on('$destroy', function() {
-      socket.unsyncUpdates('thing');
-    });
-  }
-
-  addThing() {
-    if (this.newThing) {
-      this.thingsService.add({
-        name: this.newThing
-      });
-
-      this.newThing = '';
-    }
-  }
-
-  deleteThing(thing) {
-    this.thingsService.remove(thing._id);
   }
 }
 
 angular.module('obmApp')
   .controller('MainController', MainController);
-
 })();
